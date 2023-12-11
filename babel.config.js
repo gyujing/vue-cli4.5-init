96fa303f00531655d7bffb5acdb28dba637293c7
yujing.gu <yujing.gu@aispeech.com>
2023-12-11 11:56:37 +0800

    fix(all): 生成环境去除console

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+const plugins = ['@babel/plugin-proposal-optional-chaining']
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-debugger')
+  plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
+}
+
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins
 }
